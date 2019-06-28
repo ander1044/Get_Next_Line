88ba93aac2d2bed08a43ea63c7f27a21f08862e7
Anda BEN <anben@c5r6s3.wethinkcode.co.za>
2019-06-28 14:28:16 +0200

    GNL at first

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anben <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/28 14:16:10 by anben             #+#    #+#             */
+/*   Updated: 2019/06/28 14:27:10 by anben            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+
+# include <unistd.h>
+# include <fcntl.h>
+
+int	get_next_line(const int fd, char **line);
+#endif
